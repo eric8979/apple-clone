efc5075eaad0b0706db3cfadf340c2ac8ded08f9
Eric Whale <ericwhale@hackbook890.local>
2023-02-23 10:23:48 +0900

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const buttonMini = document.getElementById('buttonMini');
+const button12 = document.getElementById('button12');
+
+function miniSelected(e){
+  
+}
+
+buttonMini.addEventListener('click', miniSelected());
+
